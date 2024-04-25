7c66239e66abb7333a6b0295016e040a8f7015e8
InkCoderYmc <673994390@qq.com>
2024-04-26 02:02:50 +0800

    0425

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -4,14 +4,13 @@
 //
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand
 // for a hint.
-
-// I AM NOT DONE
-
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    // 此处考察列表切片的语法
+    // 需要注意列表切片为左闭右开
+    let nice_slice = &a[1..4];
 
     assert_eq!([2, 3, 4], nice_slice)
 }
